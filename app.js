5fc779df744d6dfeec307c6a53443b7819cdf28a
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2022-10-20 14:52:41 +0500

    PR 13.1

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
