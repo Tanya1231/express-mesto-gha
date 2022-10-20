f6aa97496524f68cad17f18a16cc2d46db99c32e
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2022-10-20 15:09:49 +0500

    PR 13.1

--- controllers/users.js
+++ controllers/users.js
@@ -42,7 +42,7 @@ const createUser = (req, res) => {
 
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { name, about })
     .then((user) => {
       if (user === null) {
         res.status(404).send({ message: 'User с указанным _id не найдена' });
@@ -61,7 +61,7 @@ const updateProfile = (req, res) => {
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { avatar })
     .then((user) => {
       if (user === null) {
         res.status(404).send({ message: 'User с указанным _id не найдена' });
