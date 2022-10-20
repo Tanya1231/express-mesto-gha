f2a28fdbce16a2fcc70b85d4f8c3fa4f1d443ebf
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2022-10-20 10:40:10 +0500

    PR 13.1

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,8 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  User.findById(req.params.userId).orFail(new Error('NotFound'))
+  const { userId } = req.params;
+  User.findById({ _id: userId })
     .then((user) => {
       if (user === null) {
         res.status(404).send({ message: 'User с указанным _id не найдена' });
