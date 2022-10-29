37264d8a0387f6e3071a1606732b41ab06a50a7d
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2022-10-29 16:48:26 +0500

    PR 14.1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,16 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    res.status(400).send({ message: 'Необходима авторизация' });
-  }
-  const token = authorization.replace('Bearer', '');
+  const token = req.cookie.jwt;
   let payload;
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    res.status(400).send({ message: 'Необходима авторизация' });
+    res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
