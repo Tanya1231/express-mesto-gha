9ad32d7ebecd97033c13516b98ceb8e3496b02cc
Татьяна Абдулова <tanjaabdulova@yandex.ru>
2022-11-02 12:19:01 +0500

    PR 14.1

--- routes/cards.js
+++ routes/cards.js
@@ -2,33 +2,32 @@ const express = require('express');
 const { celebrate, Joi } = require('celebrate');
 
 const cardRoutes = express.Router();
-const auth = require('../middlewares/auth');
 
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-cardRoutes.get('/cards', auth, getCards);
+cardRoutes.get('/cards', getCards);
 cardRoutes.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]+\.[a-zA-Z0-9()]+([-a-zA-Z0-9()@:%_\\+.~#?&/=#]*)/),
   }),
-}), auth, createCard);
+}), createCard);
 cardRoutes.delete('/cards/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
-}), auth, deleteCard);
+}), deleteCard);
 cardRoutes.put('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
-}), auth, likeCard);
+}), likeCard);
 cardRoutes.delete('/cards/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
-}), auth, dislikeCard);
+}), dislikeCard);
 
 module.exports = { cardRoutes };
--- routes/users.js
+++ routes/users.js
@@ -26,23 +26,25 @@ userRoutes.post('/signin', celebrate({
   }),
 }), login);
 
-userRoutes.get('/users', auth, getUsers);
-userRoutes.get('/users/me', auth, getMyInfo);
+userRoutes(auth);
+
+userRoutes.get('/users', getUsers);
+userRoutes.get('/users/me', getMyInfo);
 userRoutes.get('/users/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex(),
   }),
-}), auth, getUserById);
+}), getUserById);
 userRoutes.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
-}), auth, updateProfile);
+}), updateProfile);
 userRoutes.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]+\.[a-zA-Z0-9()]+([-a-zA-Z0-9()@:%_\\+.~#?&/=#]*)/),
   }),
-}), auth, updateAvatar);
+}), updateAvatar);
 
 module.exports = { userRoutes };
